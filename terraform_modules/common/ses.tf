6850700d82cf81ea8dc8a141aec068657cdcca3f
sinofseven <em.s.00001@gmail.com>
2024-10-18 02:00:52 +0900

    format

--- terraform_modules/common/ses.tf
+++ terraform_modules/common/ses.tf
@@ -10,7 +10,7 @@ resource "aws_ses_receipt_rule_set" "mailbox" {
 
 resource "aws_ses_receipt_rule" "mailbox" {
   depends_on    = [aws_s3_bucket_policy.mailbox]
-  name          = local.ses.receipt_rule_name  # S3 Bucket Policyで循環参照になってしまうため
+  name          = local.ses.receipt_rule_name # S3 Bucket Policyで循環参照になってしまうため
   rule_set_name = aws_ses_receipt_rule_set.mailbox.rule_set_name
   recipients    = [var.receiving_mail_domain]
   enabled       = true
